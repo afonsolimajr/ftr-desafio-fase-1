e4e85316fe1f91de668ca48a42c08f3a2d37ace1
Afonso Lima <afonsolima@gmail.com>
2025-04-19 20:43:58 -0300

    web: adicionado card-new-link

---
+++ web/src/components/card-new-link.tsx
@@ -0,0 +1,21 @@
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+export default function CardNewLink() {
+  return (
+    <Card className="w-96">
+      <CardHeader>
+        <CardTitle>Novo Link</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <Label>LINK ORIGINAL</Label>
+        <Input />
+        <Label>LINK ENCURTADO</Label>
+        <Input placeholder="brev.ly/" />
+        <Button>Salvar Link</Button>
+      </CardContent>
+    </Card>
+  );
+}
