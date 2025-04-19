c89094a7ad24bf3f2d5173d56f97209eb58ddec7
Afonso Lima <afonsolima@gmail.com>
2025-04-19 20:18:08 -0300

    adicionado tema azul do shadcn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="flex flex-col w-full h-screen items-center pt-3">
       <h1 className="text-3xl font-bold">Desafio fase 1</h1>
+      <Button>Salvar link</Button>
     </div>
   );
 }
