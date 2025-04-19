a3024b41708203bed25e8ca3b7fbf03f26da20cb
Afonso Lima <afonsolima@gmail.com>
2025-04-19 20:01:03 -0300

    codigo inicial do frontend com vite e shadcn UI

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col w-full h-screen items-center pt-3">
+      <h1 className="text-3xl font-bold">Desafio fase 1</h1>
+    </div>
+  );
+}
+
+export default App;
